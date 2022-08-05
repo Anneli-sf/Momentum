c3d70b62e1d13a0c32fa80c1839c3a98835a4052
Anneli Savchenko <foranneli@gmail.com>
2022-08-05 15:45:05 +0200

    feat: added sleder

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,15 +1,14 @@
-import { showGreeting, getTimesOfDay} from "./js/greeting";
+import { showGreeting, getTimesOfDay } from "./js/greeting";
 import { showDate } from "./js/date";
 import { setLocalStorage, getLocalStorage } from "./js/local-storage";
-import { getRandomNum } from "./js/random-num"; 
-
+import { getRandomNum } from "./js/random-num";
 
 const TIME = document.querySelector(".time");
 const DATE = document.querySelector(".date");
 const GREETING = document.querySelector(".greeting");
-const BODY = document.querySelector('body');
-
-
+const BODY = document.querySelector("body");
+const SLIDE_NEXT = document.querySelector(".slide-next");
+const SLIDE_PREV = document.querySelector(".slide-prev");
 
 //----------------------------when the page load---------------
 window.addEventListener("DOMContentLoaded", loadPage);
@@ -23,29 +22,52 @@ function loadPage() {
 function showTime() {
   const date = new Date();
   const options = { hour12: false };
-  const currentTime = date.toLocaleTimeString('en-US', options);
+  const currentTime = date.toLocaleTimeString("en-US", options);
   TIME.textContent = currentTime;
   showDate(DATE);
   showGreeting(GREETING);
   setTimeout(showTime, 1000);
 }
 
-
 //-------------------LOCAL STORAGE-----------------
 
-window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener("beforeunload", setLocalStorage);
 
-window.addEventListener('load', getLocalStorage);
-
+window.addEventListener("load", getLocalStorage);
 
 //-------------------BG IMAGE-----------------
+let randomNum = getRandomNum(1, 20); //type Number
+let bgImageNum;
+function getImageNumber() {
+
+    if (randomNum < 10) bgImageNum = randomNum.toString().padStart(2, 0);
+    else bgImageNum = randomNum.toString();
+    console.log(bgImageNum);
+    return bgImageNum; //type String
+}
+
 
 function setBgImage() {
-    
-    let timesOfDay = getTimesOfDay();
-    let bgImageNum = getRandomNum(1, 20).toString();
+  let timesOfDay = getTimesOfDay();
+  getImageNumber();
+  BODY.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timesOfDay}/${bgImageNum}.jpg')`;
+}
 
-        if (bgImageNum < 10) bgImageNum = bgImageNum.toString().padStart(2, 0);
 
-    BODY.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timesOfDay}/${bgImageNum}.jpg')`;
+//-------------------SLIDER-----------------
+SLIDE_NEXT.addEventListener("click", slideNextImg);
+SLIDE_PREV.addEventListener("click", slidePrevImg);
+
+function slideNextImg() {
+    randomNum = randomNum + 1; //type Number
+    if (randomNum == 21) randomNum = 1;
+    console.log(randomNum);
+    setBgImage();
+}
+
+function slidePrevImg() {
+    randomNum = randomNum - 1; //type Number
+    if (randomNum == 0) randomNum = 20;
+    console.log(randomNum);
+    setBgImage();
 }
--- momentum/src/js/random-num.js
+++ momentum/src/js/random-num.js
@@ -1,5 +1,7 @@
 export function getRandomNum(min, max) {
+    let randomResult;
     min = Math.ceil(min);
     max = Math.floor(max);
+    
     return Math.floor(Math.random() * (max - min + 1)) + min; //max and min are included
 };
