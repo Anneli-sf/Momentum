08eb53a7c38230e0d96a63b7d419294c22a59747
Anneli Savchenko <foranneli@gmail.com>
2022-08-09 15:46:05 +0200

    feat: added weather descr, temperature, icon

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -47,15 +47,16 @@ function getImageNumber() {
 }
 
 function setBgImage() {
-  const img = new Image();
-  img.src = "images/img/bg.jpg";
+//   const img = new Image(); //если так, то подвисает загрузка
+//   img.src = "images/img/bg.jpg";
 
   let timesOfDay = getTimesOfDay();
   getImageNumber();
 
-  img.onload = () => {
-    BODY.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timesOfDay}/${bgImageNum}.jpg')`;
-  };
+  BODY.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timesOfDay}/${bgImageNum}.jpg')`;
+//   img.onload = () => {
+//     BODY.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timesOfDay}/${bgImageNum}.jpg')`;
+//   };
 }
 
 //-------------------SLIDER-----------------
@@ -75,3 +76,22 @@ function slidePrevImg() {
 
   setBgImage();
 }
+
+//--------------------WEATHER-------------
+const linkWeather = `url("https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=3d3089a958d144a6b08451c705f4ef59&units=metric")`
+const WEATHER_ICON = document.querySelector('.weather-icon');
+const TEMPERATURE = document.querySelector('.temperature');
+const WEATHER_DISCR = document.querySelector('.weather-description');
+
+async function getWeather() {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&lang=en&appid=3d3089a958d144a6b08451c705f4ef59&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
+
+    WEATHER_ICON.classList.add(`owf-804`);
+    TEMPERATURE.textContent = `${Math.floor(data.main.temp)} °C`;
+    WEATHER_DISCR.textContent = data.weather[0].description;
+};
+
+getWeather();
